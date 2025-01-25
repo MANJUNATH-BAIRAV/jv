c75bc2cb7879fb8aedd2855832ebe6958883d736
MANJUNATH-BAIRAV <manjunathbairav1@gmail.com>
2025-01-25 09:27:30 +0530

    push

---
+++ animall1.java
@@ -0,0 +1,26 @@
+class animalm
+{
+String name;
+int age;
+
+	public animalm(String aniname, int age)
+	{
+	this.name = aniname;
+	this.age = age;
+	}
+
+	public void display()
+ 	{
+	System.out.println(" The animal is " +name);
+	System.out.println(" The age is " +age);
+	}
+}
+
+public class animall1
+{
+public static void main(String[] args)
+{
+animalm objanimal = new animalm("Lion",12);
+objanimal.display();
+}
+}
